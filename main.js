80a443430e76c90f462baf585acdf6f8a797882c
COOL- <87934102+msajo@users.noreply.github.com>
2023-02-01 17:25:44 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -33,3 +33,44 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speech_data_1 + speech_data_2);
     synth.speak(utterThis);
 }
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img,got_result);
+}
+
+function got_result(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("emotion_result1").innerHTML = results[0].label;
+        document.getElementById("emotion_result2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "peace"){
+            document.getElementById("Hand_Result").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "hand split"){
+            document.getElementById("Hand_Result").innerHTML = "&#128406;";
+        }
+        if(results[0].label == "crazy"){
+            document.getElementById("Hand_Result").innerHTML = "&#129304;";
+        }
+
+        if(results[0].label == "peace"){
+            document.getElementById("Hand_Result1").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "hand split"){
+            document.getElementById("Hand_Result1").innerHTML = "&#128406;";
+        }
+        if(results[0].label == "crazy"){
+            document.getElementById("emoji_result1").innerHTML = "&#129304;";
+        }  
+
+    }
+    
+}
+
